92ddd4488e5c836e20e7c667d67a5b7abaa60de3
Alex Maldonado <alex@oasci.org>
2023-12-19 23:28:45 -0500

    Add amber test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,66 @@
+import importlib
 import os
 
 import pytest
+import requests
 
 from atomea import enable_logging
+from atomea.schema import Atomea
 
 TEST_DIR = os.path.dirname(__file__)
+CACHE_DIR = os.path.join(TEST_DIR, "cache")
+
+
+def download_file(url, local_path):
+    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+    response = requests.get(url, timeout=10, stream=True)
+    if response.status_code == 200:
+        with open(local_path, "wb") as f:
+            f.write(response.content)
+    else:
+        raise RuntimeError(f"Failed to download file at {url}")
+
+
+def download_files(dest, base_url, paths):
+    paths = [os.path.join(dest, p) for p in paths]
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+        urls = [os.path.join(base_url, p) for p in paths]
+        for url, path in zip(urls, paths):
+            download_file(url, path)
+    return paths
+
+
+@pytest.fixture
+def test_dir():
+    return os.path.abspath(TEST_DIR)
 
 
 @pytest.fixture(scope="session", autouse=True)
 def turn_on_logging():
     enable_logging(10)
+
+
+@pytest.fixture
+def base_atomea():
+    schema_dir = importlib.resources.files("atomea.schemas")
+    schema_paths = []
+    for file_path in schema_dir.iterdir():
+        if file_path.suffix == ".yaml" or file_path.suffix == ".yml":
+            schema_paths.append(file_path)
+    return Atomea(schema_paths)
+
+
+@pytest.fixture
+def globus_simlify_url():
+    return "https://g-9e1ff7.1d26db.e229.dn.glob.us"
+
+
+@pytest.fixture
+def uuid_simlify_rogfp2(globus_simlify_url):
+    uuid = "f7498a8c-d021-491c-a343-10151e81434a"
+    base_url = os.path.join(globus_simlify_url, uuid)
+    dest = os.path.join(CACHE_DIR, uuid)
+    slugs = ["topo/mol.prmtop", "outputs/07_relax_npt.nc"]
+    download_files(dest, base_url, slugs)
+    return dest
---
+++ tests/test_amber.py
@@ -0,0 +1,15 @@
+"""Processing AMBER simulations with roGFP2"""
+import os
+
+from atomea.digesters import MDAnalysisDigester
+
+
+def test_amber_rogfp2_digest(uuid_simlify_rogfp2, base_atomea):
+    digester = MDAnalysisDigester()
+    topo_path = os.path.join(uuid_simlify_rogfp2, "topo/mol.prmtop")
+    coord_path = os.path.join(uuid_simlify_rogfp2, "outputs/07_relax_npt.nc")
+
+    schema = base_atomea.get()
+    keep_keys = "coordinates"
+    base_atomea.schema = {key: schema[key] for key in schema if key in keep_keys}
+    data = digester.digest(base_atomea, topo_path, coord_path)
