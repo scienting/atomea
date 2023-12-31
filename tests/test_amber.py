17c2226cbff743c3de0510472aa9925aa0812602
Alex Maldonado <alex@oasci.org>
2023-12-31 17:44:13 -0500

    Update tests

--- tests/test_amber.py
+++ tests/test_amber.py
@@ -10,18 +10,18 @@ from atomea.io import DiskData
 
 
 def test_amber_rogfp2_digest(uuid_simlify_rogfp2, base_atomea):
-    digester = MDAnalysisDigester()
     topo_path = os.path.join(uuid_simlify_rogfp2, "topo/mol.prmtop")
     coord_path = os.path.join(uuid_simlify_rogfp2, "outputs/07_relax_npt.nc")
 
     base_atomea.filter(("coordinates", "ff_atom_type"))
-    data = digester.digest(base_atomea, topo_path, coord_path)
 
     disk = DiskData()
     with tempfile.TemporaryDirectory() as tmp_dir:
-        disk.store(tmp_dir, data, base_atomea)
+        disk.store(
+            tmp_dir, base_atomea, MDAnalysisDigester, (topo_path, coord_path), {}
+        )
 
         table_test = pq.read_table(os.path.join(tmp_dir, "atoms.parquet"))
 
         assert table_test.column("ff_atom_type").type.equals(pa.string())
-        assert table_test.shape == (373810, 1)
+        assert table_test.shape == (37381, 1)
