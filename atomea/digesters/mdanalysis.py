aee34e71e0e4f186f2809852d8c9334ff86f81fa
Alex Maldonado <alex@oasci.org>
2024-05-24 10:13:27 -0400

    fix: MDAnalysis Digester

--- atomea/digesters/mdanalysis.py
+++ atomea/digesters/mdanalysis.py
@@ -4,6 +4,7 @@ from collections.abc import Collection
 
 import numpy as np
 import numpy.typing as npt
+from loguru import logger
 
 try:
     import MDAnalysis as mda
@@ -27,6 +28,12 @@ class MDAnalysisDigester(Digester):
 
     @staticmethod
     def checks():
+        """Perform checks to ensure MDAnalysis is installed.
+
+        Raises:
+            ImportError: If MDAnalysis is not installed.
+        """
+        logger.info("Checking MDAnalysis installation")
         if not HAS_MDANALYSIS:
             raise ImportError("MDAnalysis is not installed")
 
@@ -34,19 +41,62 @@ class MDAnalysisDigester(Digester):
     def prepare_step_inputs(
         cls, *args: Any, **kwargs: Collection[Any]
     ) -> dict[str, mda.Universe]:
-        inputs = {"u": mda.Universe(*args, **kwargs)}
+        """Prepare and return the inputs necessary for the MDAnalysis digestion process.
+
+        This method loads the MDAnalysis Universe using the provided arguments and keyword
+        arguments and prepares the inputs for digestion.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            dict[str, mda.Universe]: A dictionary containing the MDAnalysis Universe.
+        """
+        logger.info("Preparing inputs for MDAnalysis digesters.")
+        logger.debug(f"Loading universe with {args} and {kwargs}")
+        if "coordinates" in kwargs.keys():
+            logger.warning("Coordinates must be passed in args not kwargs")
+            logger.warning(
+                "Dropping coordinates from kwargs to avoid MDAnalysis error."
+            )
+            kwargs.pop("coordinates")
+        u = mda.Universe(*args, **kwargs)
+        logger.debug(f"MDAnalysis universe with {len(u.atoms)} atoms.")
+        inputs = {"u": u}
         return inputs
 
     @staticmethod
-    def coordinates(u: mda.Universe) -> npt.NDArray[np.float64]:
-        """Return the coordinates of the atoms in the universe."""
+    def coordinates(u: mda.Universe) -> dict[str, npt.NDArray[np.float64]]:
+        """Return the coordinates of the atoms in the MDAnalysis Universe.
+
+        Args:
+            u (mda.Universe): The MDAnalysis Universe containing the atoms.
+
+        Returns:
+            dict[str, npt.NDArray[np.float64]]: A dictionary with the key
+                "system.coordinates" and the value being a numpy array of atom
+                coordinates.
+
+        Raises:
+            ValueError: If the coordinates are not a numpy array.
+        """
         v = u.atoms.positions
         if isinstance(v, np.ndarray):
-            return v
-        raise TypeError(f"{type(v)} is not a numpy array")
+            return {"system.coordinates": v}
+        else:
+            raise ValueError("Coordinates must be a numpy array.")
 
     @staticmethod
-    def ff_atom_type(u: mda.Universe) -> list[str]:
-        """Return the coordinates of the atoms in the universe."""
+    def ff_atom_type(u: mda.Universe) -> dict[str, list[str]]:
+        """Return the force field atom types of the atoms in the MDAnalysis Universe.
+
+        Args:
+            u (mda.Universe): The MDAnalysis Universe containing the atoms.
+
+        Returns:
+            dict[str, list[str]]: A dictionary with the key "topology.ff_atom_type"
+                and the value being a list of atom types.
+        """
         atom_types: list[str] = u.atoms.accumulate("types", function=accumulate_things)
-        return atom_types
+        return {"topology.ff_atom_type": atom_types}
