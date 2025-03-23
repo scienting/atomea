1557182bc93a0d016b89194f0058a7d4e1a223f9
Alex Maldonado <alex@oasci.org>
2025-03-23 12:51:31 -0400

    refactor: fix init for workflow schemas

--- atomea/schemas/workflow/__init__.py
+++ atomea/schemas/workflow/__init__.py
@@ -1,4 +1,13 @@
 from .ff import ForcefieldSchemaBase
+from .run import RunConfig
 from .slurm import SlurmSchema
+from .solution import SolutionConfig
+from .topo import TopologyConfig
 
-__all__ = ["ForcefieldSchemaBase", "SlurmSchema"]
+__all__ = [
+    "ForcefieldSchemaBase",
+    "SlurmSchema",
+    "TopologyConfig",
+    "SolutionConfig",
+    "RunConfig",
+]
